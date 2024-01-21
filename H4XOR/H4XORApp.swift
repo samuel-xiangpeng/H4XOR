0235d4a1d73f484d0597274a7195f2b982f2ca1d
samuel-xiangpeng <samuel_xiangpeng@163.com>
2024-01-21 10:47:28 +0800

    Initial Commit

---
+++ H4XOR/H4XORApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XORApp.swift
+//  H4XOR
+//
+//  Created by 周祥鹏 on 2024/1/21.
+//
+
+import SwiftUI
+
+@main
+struct H4XORApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
